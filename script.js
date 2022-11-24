df63ff81576892e7f46a78b17cc04a741ac928b8
WAM <eraserw@gmail.com>
2022-11-24 10:17:54 +0100

    fix:all rendu finalV1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('input');
 const form = document.getElementById('input-form');
-const nomVille = document.getElementById('nomVille');
+//const nomVille = document.getElementById('nomVille'); remplacer par sstitre dans le titre de page !
 const temp = document.getElementById('temp');
 const tempMin = document.getElementById('tempMin');
 const tempMax = document.getElementById('tempMax');
@@ -12,7 +12,7 @@ const refIcon = document.getElementById('imgMeteo');
 
 const KELVIN = 273.15;
 
-
+const ssTitre = document.getElementById('sstitre');
 
 btn.addEventListener('click', event => {
     event.preventDefault();
@@ -28,9 +28,9 @@ form.addEventListener('submit', event => {
 
 //fct qui avec le nom de la ville et le code pays(FR) permet d'avoir sa position gps, via API !!
 function fctCoord(ville) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ville},FR&limit=1&appid=147cfa8444d4c4443f5fe7207c42d9bb`) // fletch accepte string en entrée !!
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ville},FR&limit=1&appid=147cfa8444d4c4443f5fe7207c42d9bb`) // fletch accepte string en entrée !! si direct? par weather? !!
         .then((response) => response.json())
-        .then((data) => weatherMaster(data[0].lat,data[0].lon))
+        .then((data) => weatherMaster(data[0].lat, data[0].lon))
         .catch((error) => { console.log(error) })
 };
 
@@ -54,17 +54,18 @@ function affMeteo(data) {
     let temP_max = (data.main.temp_max - KELVIN).toFixed(2);
     let vitesseKMH = (data.wind.speed * 1.609344).toFixed(2);
     let iconRef = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    
+
     girouette(data.wind.deg);
 
-    nomVille.textContent = `Actuellement, le climat de ${data.name} est :`;
+    ssTitre.textContent = `${data.name}`;
+    /* nomVille.textContent = `Actuellement, le climat de ${data.name} est :`; idem que pour la constante */
     temp.textContent = `La température ressentie est de ${temP}°C`;
     tempMin.textContent = `La température ressentie est de ${temP_min}°C`;
     tempMax.textContent = `La température ressentie est de ${temP_max}°C`;
-    humidity.textContent = ` est de ${data.main.humidity}%`;
+    humidity.textContent = `L'humidité de l'air est de ${data.main.humidity}%`;
     ventVitess.textContent = `La vitesse du vent est de ${vitesseKMH}Km/h`;
     ventSens.textContent = `La direction du vent est : ${varSwitch}`;
-    refIcon.setAttribute("src",iconRef);
+    refIcon.setAttribute("src", iconRef);
     console.log(varSwitch);
     console.log(girouette(55));
     console.log(iconRef);
@@ -74,41 +75,45 @@ function affMeteo(data) {
 }
 let varSwitch = "";
 function girouette(nb) {
-    x=parseInt(nb);
+    x = parseInt(nb);
     switch (true) {
         case x == 0: return varSwitch = "N(nord)";
-            
-        case  x<45: return varSwitch = "NNE(nord - nord - est)";
-            
-        case x==45: return varSwitch = "NE(nord - est)";
-            
-        case x<68: return varSwitch = "ENE(est - nord - est)";
-            
-        case x==90: return varSwitch = "E(est)";
-            
-        case x<113: return varSwitch = "ESE(est - sud - est)";
-            
-        case x==135: return varSwitch = "SE(sud - est)";
-            
-        case x<158: return varSwitch = "SSE(sud - sud - est)";
-            
-        case x==180: return varSwitch = "S(sud)";
-            
-        case x<203: return varSwitch = "SSO(sud - sud - ouest)";
-            
-        case x==225: return varSwitch = "SO(sud - ouest)";
-            
-        case x<248:return varSwitch = "OSO(ouest - sud - ouest)";
-            
-        case x==270: return varSwitch = "O(ouest)";
-            
-        case x<293: return varSwitch = "ONO(ouest - nord - ouest)";
-            
-        case x==315: return varSwitch = "NO(nord - ouest)";
-            
-        case x<338: return varSwitch = "NNO(nord - nord - ouest)";
-            
+
+        case x < 45: return varSwitch = "NNE(nord - nord - est)";
+
+        case x == 45: return varSwitch = "NE(nord - est)";
+
+        case x < 68: return varSwitch = "ENE(est - nord - est)";
+
+        case x == 90: return varSwitch = "E(est)";
+
+        case x < 113: return varSwitch = "ESE(est - sud - est)";
+
+        case x == 135: return varSwitch = "SE(sud - est)";
+
+        case x < 158: return varSwitch = "SSE(sud - sud - est)";
+
+        case x == 180: return varSwitch = "S(sud)";
+
+        case x < 203: return varSwitch = "SSO(sud - sud - ouest)";
+
+        case x == 225: return varSwitch = "SO(sud - ouest)";
+
+        case x < 248: return varSwitch = "OSO(ouest - sud - ouest)";
+
+        case x == 270: return varSwitch = "O(ouest)";
+
+        case x < 293: return varSwitch = "ONO(ouest - nord - ouest)";
+
+        case x == 315: return varSwitch = "NO(nord - ouest)";
+
+        case x < 338: return varSwitch = "NNO(nord - nord - ouest)";
+
         default: varSwitch = "N(nord)";
     };
     return varSwitch;
 };
+
+
+
+//input.addEventListener('cuechange')
